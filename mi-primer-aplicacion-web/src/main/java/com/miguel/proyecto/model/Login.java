1fb2ddca823c063e6a6c873ba47f365ddb1ed300
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-03-06 15:17:24 -0600

    Agrega clases de JPA.

---
+++ mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/model/Login.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miguel.proyecto.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author miguel
+ */
+@Entity
+@Table(catalog = "ejemplo", schema = "login", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"usuario"})})
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Login.findAll", query = "SELECT l FROM Login l")
+    , @NamedQuery(name = "Login.findById", query = "SELECT l FROM Login l WHERE l.id = :id")
+    , @NamedQuery(name = "Login.findByUsuario", query = "SELECT l FROM Login l WHERE l.usuario = :usuario")
+    , @NamedQuery(name = "Login.findByPass", query = "SELECT l FROM Login l WHERE l.pass = :pass")})
+@NamedNativeQueries(value = {
+    @NamedNativeQuery(
+            name = "Login.canLogin",
+            query = "select login.login(?, ?)"
+    )
+})
+public class Login implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 2147483647)
+    private String usuario;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 2147483647)
+    private String pass;
+
+    public Login() {
+    }
+
+    public Login(Integer id) {
+        this.id = id;
+    }
+
+    public Login(Integer id, String usuario, String pass) {
+        this.id = id;
+        this.usuario = usuario;
+        this.pass = pass;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Login)) {
+            return false;
+        }
+        Login other = (Login) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.miguel.proyecto.model.Login[ id=" + id + " ]";
+    }
+
+}
---
+++ mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/model/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.miguel.proyecto.model.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
