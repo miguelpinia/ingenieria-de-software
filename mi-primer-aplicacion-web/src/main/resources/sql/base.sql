dcd47f6cb8bb9d4c286dbef72b5b65c7c8406212
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-04-09 19:34:01 -0500

    Agrega modelo de ejemplo de preguntas a SQL.

--- mi-primer-aplicacion-web/src/main/resources/sql/base.sql
+++ mi-primer-aplicacion-web/src/main/resources/sql/base.sql
@@ -50,4 +50,40 @@ $$ language sql stable;
 
 insert into login.login (usuario, password) values ('Miguel', 'password');
 
+drop table if exists public.usuario cascade;
+
+create table public.usuario (
+  id serial primary key,
+  login_id int not null references login.login(id),
+  nombre text not null,
+  correo text not null,
+  constraint correo_unico unique(correo),
+  constraint email_valido check (correo ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+);
+
+insert into public.usuario (login_id, nombre, correo) values (1, 'miguel', 'miguel_pinia@ciencias.unam.mx');
+
+drop table if exists public.pregunta cascade;
+
+create table public.pregunta (
+  id serial primary key
+  , usuario_id int not null references public.usuario(id)
+  , contenido text not null
+);
+
+insert into public.pregunta (usuario_id, contenido) values (1, 'Mi primer pregunta'),
+                                                           (1, 'Mi segunda pregunta');
+
+drop table if exists public.respuesta;
+
+create table public.respuesta (
+  id serial primary key
+  , usuario_id int not null references public.usuario(id)
+  , pregunta_id int not null references public.pregunta(id)
+  , contenido text not null
+);
+
+insert into public.respuesta (usuario_id, pregunta_id, contenido) values (1, 1, 'Respuesta 1'),
+                                                                         (1, 1, 'Respuesta 2'),
+                                                                         (1, 2, 'Respuesta 1 pregunta 2');
 commit;
