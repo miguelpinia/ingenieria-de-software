a71868f8aaf296b9cff7e3754b339512c252d9f0
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-09-03 15:29:08 -0500

    Sube código de la primera aplicación web.

---
+++ mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/web/Usuario.java
@@ -0,0 +1,32 @@
+package com.miguel.proyecto.web;
+
+public class Usuario {
+
+    private String usuario;
+    private String contraseña;
+    private String confirmacionContraseña;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getConfirmacionContraseña() {
+        return confirmacionContraseña;
+    }
+
+    public void setConfirmacionContraseña(String confirmacionContraseña) {
+        this.confirmacionContraseña = confirmacionContraseña;
+    }
+}
