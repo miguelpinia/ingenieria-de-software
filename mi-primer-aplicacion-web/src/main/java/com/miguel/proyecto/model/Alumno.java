d23c0781e5fea1ca5d782dca5c8ee31d5612f443
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-09-17 15:21:22 -0500

    Agrega clases de JPA y unidad de persistencia.

---
+++ mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/model/Alumno.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miguel.proyecto.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author miguel
+ */
+@Entity
+@Table(catalog = "ejemplo", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumno.findAll", query = "SELECT a FROM Alumno a")
+    , @NamedQuery(name = "Alumno.findById", query = "SELECT a FROM Alumno a WHERE a.id = :id")
+    , @NamedQuery(name = "Alumno.findByNombre", query = "SELECT a FROM Alumno a WHERE a.nombre = :nombre")
+    , @NamedQuery(name = "Alumno.findByApellido1", query = "SELECT a FROM Alumno a WHERE a.apellido1 = :apellido1")
+    , @NamedQuery(name = "Alumno.findByCorreo", query = "SELECT a FROM Alumno a WHERE a.correo = :correo")})
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 2147483647)
+    private String nombre;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 2147483647)
+    private String apellido1;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 2147483647)
+    private String correo;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idAlumno")
+    private List<Profesor> profesorList;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer id) {
+        this.id = id;
+    }
+
+    public Alumno(Integer id, String nombre, String apellido1, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.correo = correo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @XmlTransient
+    public List<Profesor> getProfesorList() {
+        return profesorList;
+    }
+
+    public void setProfesorList(List<Profesor> profesorList) {
+        this.profesorList = profesorList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumno)) {
+            return false;
+        }
+        Alumno other = (Alumno) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.miguel.proyecto.model.Alumno[ id=" + id + " ]";
+    }
+
+}
---
+++ mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/model/Profesor.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miguel.proyecto.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author miguel
+ */
+@Entity
+@Table(catalog = "ejemplo", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Profesor.findAll", query = "SELECT p FROM Profesor p")
+    , @NamedQuery(name = "Profesor.findById", query = "SELECT p FROM Profesor p WHERE p.id = :id")})
+public class Profesor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @JoinColumn(name = "id_alumno", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private Alumno idAlumno;
+
+    public Profesor() {
+    }
+
+    public Profesor(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Alumno getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Alumno idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Profesor)) {
+            return false;
+        }
+        Profesor other = (Profesor) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.miguel.proyecto.model.Profesor[ id=" + id + " ]";
+    }
+
+}
